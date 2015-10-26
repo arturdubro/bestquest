e64b9988e4f0fb10c31c2462a4582428843eb439
Roma Lapin <roman.lapin@gmail.com>
2015-10-26 22:45:42 +0300

    add admin and move copy static to web directory

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git archive --format=tar origin/gh-pages | gzip -9c | ssh deployer@188.226.163.161 "cd /var/www/php/best-quest.ru; tar xvzf -"
+git archive --format=tar origin/gh-pages | gzip -9c | ssh deployer@188.226.163.161 "cd /var/www/php/bqbs; tar xvzf -"
