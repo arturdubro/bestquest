8c78c7d7fd496a39e080b1633161685547bc5f1e
Dmitrij Konstantinov <kod2012@gmail.com>
2016-07-22 15:14:17 +0300

    change server ip

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git archive --format=tar origin/master | gzip -9c | ssh deployer@188.226.163.161 "cd /var/www/php/bqbs; tar xvzf -"
+git archive --format=tar origin/master | gzip -9c | ssh deployer@85.143.221.253 "cd /var/www/php/bqbs; tar xvzf -"
